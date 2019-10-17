1a388682bdec0c7ea16803b109e001dffa97df86
Darko Kukovec <darko@infinum.hr>
2019-10-17 21:29:57 +0200

    Update dependencies

--- dist/inject.d.ts
+++ dist/inject.d.ts
@@ -1,6 +1,4 @@
-import { Context } from 'react';
 import { ScopeToken } from './ScopeToken';
 import { TKey } from './types';
 export declare function useInject<T>(cls: TKey<T>, scope?: ScopeToken): T;
-export declare function useContextInject<T>(cls: TKey<T>, contextKey: Context<ScopeToken>): T;
 export declare function inject<T>(cls: TKey<T>, scope?: ScopeToken): T;
